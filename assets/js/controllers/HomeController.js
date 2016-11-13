9b32bbd89533a716ba9b2d0fb08fed760dc8cf72
Christine Lau <chylau.design@gmail.com>
2016-11-12 19:51:23 -0800

    started styling the panels

--- assets/js/controllers/HomeController.js
+++ assets/js/controllers/HomeController.js
@@ -3,6 +3,9 @@ angular.module('algolia')
 
 HomeController.$inject=['$scope', 'algolia'];
 function HomeController( $scope,   algolia ) {
+  var client = algolia.Client( "J31118LB7W", "805c64911a7abd9b7a02ac3fe9f28d00");
+  var index = client.initIndex('restaurants');
+
   var vm = this;
   vm.hello = "Helloooo";
 
@@ -10,17 +13,15 @@ function HomeController( $scope,   algolia ) {
         query: '',
         hits: []
       };
-    var client = algolia.Client( "J31118LB7W", "805c64911a7abd9b7a02ac3fe9f28d00");
-    var index = client.initIndex('restaurants');
 
-      $scope.$watch('search.query', function() {
-        index.search($scope.search.query)
-          .then(function searchSuccess(content) {
-            console.log("CONTENT", content);
-            // add content of search results to scope for display in view
-            $scope.search.hits = content.hits;
-          }, function searchFailure(err) {
-            console.log(err);
-        });
+    $scope.$watch('search.query', function() {
+      index.search($scope.search.query)
+        .then(function searchSuccess(content) {
+          console.log("CONTENT", content);
+          // add content of search results to scope for display in view
+          $scope.search.hits = content.hits;
+        }, function searchFailure(err) {
+          console.log(err);
       });
+    });
 }
