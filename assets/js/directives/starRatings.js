231f8a45875ba3fd13ed1a9b8b3044596b10dbdd
Christine Lau <chylau.design@gmail.com>
2016-11-15 01:15:03 -0800

    added stars to the side bar and adjusted the styling

--- assets/js/directives/starRatings.js
+++ assets/js/directives/starRatings.js
@@ -1,17 +1,14 @@
 angular.module('algolia')
 .directive('starRatings', starRatings);
-
 function starRatings(){
   var directive = {
     restrict: 'E',
     require: "resultCard",
-    replace: true,
     templateUrl:  "templates/starRatings.html",
     controller: starRatingsController,
     controllerAs: 'starRatingsCtrl',
     scope: {
       stars: '=stars',
-      reviewCount: '=reviews'
     }
   };
   return directive;
@@ -19,17 +16,16 @@ function starRatings(){
 
 function starRatingsController($scope){
   var vm  = this;
-  var Rstars = Math.round($scope.stars)
+  var Rstars = Math.round($scope.stars);
   $scope.ratings = {
     current: Rstars,
     empty: 5 - Rstars
-  }
+  };
   $scope.range = function(count){
     var ratings = [];
     for (var i = 1; i <= count; i++) {
-      ratings.push(i)
+      ratings.push(i);
     }
     return ratings;
-    }
-    console.log("revien", $scope.reviewCount);
+  };
 }
